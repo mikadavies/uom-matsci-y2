294c8aa8f871bbe299dc708804e87364c7dd1222
Mika Davies <77616015+mikadavies@users.noreply.github.com>
2021-10-07 22:37:55 +0100

    Flexible Tensors - Tensors of any rank and dimensions can now be created

--- MATS23101 Mechanics of Materials/continuum_mechanics.py
+++ MATS23101 Mechanics of Materials/continuum_mechanics.py
@@ -2,32 +2,40 @@
 
 import numpy as np
 
+alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" #allows 52 rank tensors?
+
 class Tensor():
     def __init__(self, rank=2, max_index=3):
         self.rank = rank
         self.max = max_index
         self.tensor = None
     
-    def _nest(self, layers, length):
+    def _nest(self, layers:int, length:int) -> list:
         """
-            Nests a certain amount of lists (length) in a certain amount of layers (layers) 
-            
-            Input:
-                layers: int
-                length: int
-                
-            Output: list of lists
+        Nests a certain amount of lists (length) in a certain amount of layers (layers) 
         """
-        return [[] if layers == 1 else self._nest(layers-1, length) for _ in range(length)]
+        return [[] if layers == 0 else self._nest(layers-1, length) for _ in range(length)]
     
-    def create(self, values:list):
+    def create(self, values:list) -> list:
         """
-            Creates a readable and usable tensor representation given a list of values
-        
-            Input:
-                values: list    // format [11, 12, 13 ..., 21, 22, 23...]
+        Creates a readable and usable tensor representation given a list of values
         """
-        shell = self._nest(self.rank-1, self.max)
-        self.tensor = shell
-        return self.tensor #TODO: currently returns an empty 'Tensor', with the correct rank
-        
+        temp_arr = np.array_split(values, len(values)/self.max) #creates the dimensions
+        temp_arr = [list(x) for x in temp_arr]
+        ##print(temp_arr)
+        shell = self._nest(self.rank, self.max) #empty tensor
+        tensor = shell
+        #to make an self-changing code dependant on the tensor rank
+        #I used strings, as they can be iterably modified
+        access_code = "tensor"
+        for _ in range(0, self.rank-1):
+            access_code = access_code + "[" + alphabet[_] + "]" #determines which dimension to access
+        access_command = "for a in range(0, self.max):"
+        for _ in range(1, self.rank-1): 
+            access_command = access_command +"\n" + "    "*_ +"for " + alphabet[_] + " in range(0, self.max):" #iterates through the dimensions regardless of nesting
+        cmd = "it=0\n"+ access_command  + "\n" + "    "*(self.rank-1) + access_code + " = temp_arr[it]\n"+"    "*(self.rank-1)+"it+=1"
+        ##print(cmd)
+        exec(cmd)
+        self.tensor = tensor
+        return self.tensor
+
